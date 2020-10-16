4609dc25de99dab0643c7b0c910823fd53b1ca19
Johanna <svallingson@gmail.com>
2020-10-16 11:43:19 +0200

    refactored triangle class

--- src/main/java/triangle/Triangle.java
+++ src/main/java/triangle/Triangle.java
@@ -1,5 +1,8 @@
 package triangle;
 
+import java.util.stream.DoubleStream;
+
+
 public class Triangle {
 
     double[] sides;
@@ -9,52 +12,33 @@ public class Triangle {
     }
 
     public Triangle(double[] s) {
-        sides = new double[s.length];
-        System.arraycopy(s, 0, sides, 0, s.length);
+        this(s[0],s[1],s[2]);
     }
 
     public Triangle(Point a, Point b, Point c) {
         sides = new double[3];
         sides[0] = Math.sqrt(Math.pow((double) (b.x - a.x), 2.0) + Math.pow((double) (b.y - a.y), 2.0));
-        sides[1] = Math.sqrt(Math.pow((double) (b.x - c.x), 2.0) + Math.pow((double) (b.x - a.x), 2.0));
-        sides[2] = Math.sqrt(Math.pow((double) (c.x - a.x), 2.0) + Math.pow((double) (c.x - a.x), 2.0));
+        sides[1] = Math.sqrt(Math.pow((double) (b.x - c.x), 2.0) + Math.pow((double) (b.y - a.y), 2.0));
+        sides[2] = Math.sqrt(Math.pow((double) (c.x - a.x), 2.0) + Math.pow((double) (c.y - a.y), 2.0));
     }
 
     public Triangle(Point[] s) {
-        sides = new double[s.length];
-        sides[0] = Math.sqrt(Math.pow((double) (s[1].x - s[0].x), 2.0) + Math.pow((double) (s[1].y - s[0].y), 2.0));
-        sides[1] = Math.sqrt(Math.pow((double) (s[1].x - s[2].x), 2.0) + Math.pow((double) (s[1].x - s[2].x), 2.0));
-        sides[2] = Math.sqrt(Math.pow((double) (s[2].x - s[0].x), 2.0) + Math.pow((double) (s[2].x - s[0].x), 2.0));
+        this(s[0],s[1],s[2]);
     }
 
     private int uniqueSides() {
-
-        int distinctIntegers = 0;
-
-        for (int j = 0; j < sides.length; j++) {
-            double thisInt = sides[j];
-            boolean seenThisIntBefore = false;
-            for (int i = 0; i < j; i++) {
-                if (thisInt == sides[i]) {
-                    seenThisIntBefore = true;
-                }
-            }
-            if (!seenThisIntBefore) {
-                distinctIntegers++;
-            }
-        }
-        return distinctIntegers;
+        return (int)DoubleStream.of(sides).distinct().count();
     }
 
     public boolean isScalene() {
-        if (uniqueSides() == 1) {
+        if (uniqueSides() == 3) {
             return true;
         }
         return false;
     }
 
     public boolean isEquilateral() {
-        if (uniqueSides() == 3) {
+        if (uniqueSides() == 1) {
             return true;
         }
         return false;
