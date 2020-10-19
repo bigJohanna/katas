c541850aae0ffa922395135f34ee830718995399
Johanna <svallingson@gmail.com>
2020-10-19 11:46:11 +0200

    added new line as delimeter

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,14 +5,15 @@ public class StringCalculator {
     public int add(String numbers){
         int sum = 0;
 
-        String[] nums = numbers.split(",");
+        String[] nums = numbers.split(",|\\n");
         try{
-            for(String s : nums)
-            sum += Integer.parseInt(s);
+            for(String s : nums) {
+                sum += Integer.parseInt(s);
+            }
+
            }catch(IllegalArgumentException e){
                return 0;
            }
              return sum;
-
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -8,17 +10,29 @@ class StringCalculatorTest {
     private final StringCalculator cal = new StringCalculator();
 
     @Test
-    void emptyStringShouldReturn0() {
-        int returnValue = cal.add("");
-        assertThat(returnValue == 0);
+    void givenTwoValuesWithCommaSeparationShouldReturnSum() {
+        assertThat(cal.add("1,2")).isEqualTo(3);
+
     }
 
     @Test
-    void givenTwoValuesWithCommaSeparationShouldReturnSum() {
-        int actual1 = cal.add("1,2");
-        int actual2 = cal.add("4,6");
-        assertThat(actual1 == 3);
-        assertThat(actual2 == 10);
+    void givenAStringWithUnknownAmountOfNumbersShouldReturnSum() {
+        assertThat(cal.add("2,3,4,5,6")).isEqualTo(20);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "r", "one", "2,y3", "@", "1,\\n"})
+    void givenInvaldInputShouldReturn0(String param) {
+        int actual = cal.add(param);
+        assertThat(actual).isEqualTo(0);
+    }
+
+    @Test
+    void givenInputWithNewLineAsSeparatorShouldReturnSum() {
+        assertThat(cal.add("1\n2,3")).isEqualTo(6);
+    }
+
+    @Test
+    void name() {
+    }
 }
